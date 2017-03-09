3a44c516dc85ede91c5dd518e49abb0eae7cb608
Sri Uswatul H <sriuswatulhasanah@gmail.com>
2017-03-09 15:38:17 -0800

    Tugas 2 Selesai

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('bayu',function(){
-	echo "bayu";
-}):
+
+Route::get('hello-world', function (){
+	return 'hello-World';
+});
+
+Route::get('pengguna/{pengguna}', function ($pengguna){
+	return "Hello-World dari pengguna $pengguna";
+});
+
+Route::get('kelasd2/framework/{mhs?}', function ($mhs="Anonim"){// tanda tanya buat kalo mau pake parameter ataupun enda
+	return "Selamat Datang $mhs";
+});
